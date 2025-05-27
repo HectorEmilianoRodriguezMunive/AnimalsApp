41f03804ae0e9ab676484e345c0dbf673065952e
Hector Emiliano Rodriguez Munive <rodriguezmunivehectoremiliano@gmail.com>
2025-05-27 09:38:38 -0600

    feat: spatie migrations loaded & default roles & roles

--- resources/views/filament/resources/quote-resource/pages/create-quote.blade.php
+++ resources/views/filament/resources/quote-resource/pages/create-quote.blade.php
@@ -1,15 +1,16 @@
 <x-filament-panels::page> 
 
-    <div>
         <p>
              Añade servicios a la cita
         </p>
 
+    <div>
+        
     {{$this->form}}
 
-     <x-filament::button type="submit">
-        Agregar
-    </x-filament::button>
+        <x-filament::button type="submit">
+            Agregar
+        </x-filament::button>
     </div>
     
 </x-filament-panels::page>
